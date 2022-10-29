6a85070a0cbc7cf566263a05f6e85ace0b0bc979
Justine Edward Santos <hydxenonmain@gmail.com>
2022-10-29 21:33:36 +0800

    Refactor Contents.js and Initial dist subtree commit

--- src/modules/Contents.js
+++ src/modules/Contents.js
@@ -1,156 +1,148 @@
-import {projects } from "./Project"
-import {createButton, createDiv, createI, createInput, createP} from "./createElementsDom"
+import { projects } from "./Project";
+import {
+  createButton,
+  createDiv,
+  createI,
+  createInput,
+  createP,
+} from "./createElementsDom";
 
-const contentsContainer = document.querySelector(".contents-container")
+const contentsContainer = document.querySelector(".contents-container");
 
-let addDisable = false
+let addDisable = false;
 
-const setH1Title = (title)=>{
-    const h1Title = document.createElement("h1")
-    h1Title.textContent = title
-    contentsContainer.appendChild(h1Title)
-}
+const setH1Title = (title) => {
+  const h1Title = document.createElement("h1");
+  h1Title.textContent = title;
+  contentsContainer.appendChild(h1Title);
+};
 
-const createTask = ()=>{
+const createTask = () => {
+  const taskContainer = createDiv();
+  taskContainer.classList.add("task-container");
+  contentsContainer.appendChild(taskContainer);
 
-    const taskContainer = createDiv()
-    taskContainer.classList.add("task-container")
-    contentsContainer.appendChild(taskContainer)
+  const task = createDiv();
+  task.classList.add("task");
+  taskContainer.appendChild(task);
 
-    const task = createDiv()
-    task.classList.add("task")
-    taskContainer.appendChild(task)
+  const plus = createI();
+  plus.classList.add("fa-solid", "fa-plus");
 
-    const plus = createI()
-    plus.classList.add("fa-solid","fa-plus")
+  const input = createInput();
+  input.value = "Add a new Task";
+  input.readOnly = true;
 
-    const input = createInput()
-    input.value = "Add a new Task"
-    input.readOnly = true
+  task.append(plus, input);
 
-    task.append(plus,input)
+  const editDiv = createDiv();
+  editDiv.classList.add("edit");
+  taskContainer.appendChild(editDiv);
 
-    const editDiv = createDiv()
-    editDiv.classList.add("edit")
-    taskContainer.appendChild(editDiv)
+  const editTask = () => {
+    if (addDisable) return;
+    input.readOnly = false;
+    input.style.border = "1px solid green";
+    input.value = "";
 
+    const createAddBtn = createButton();
+    createAddBtn.classList.add("add-btn", "task-btn");
+    createAddBtn.textContent = "Add Task";
+    editDiv.appendChild(createAddBtn);
+    createAddBtn.addEventListener("click", addTask);
+    addDisable = true;
+  };
 
-    const editTask = ()=>{
-
-        if(addDisable)return
-        input.readOnly = false
-        input.style.border = "1px solid green"
-        input.value = ""
-
-        const createAddBtn = createButton()
-        createAddBtn.classList.add("add-btn","task-btn")
-        createAddBtn.textContent = "Add Task"
-        editDiv.appendChild(createAddBtn)
-        createAddBtn.addEventListener("click",addTask)
-        addDisable = true
+  const addTask = () => {
+    if (input.value == "") {
+      input.style.border = "1px solid red";
+      return;
     }
 
+    const clearTask = () => {
+      input.value = "Add a new task";
+      input.style.border = "none";
+      const createAddBtn = document.querySelector(".task-btn");
+      createAddBtn.parentNode.removeChild(createAddBtn);
+    };
 
-    const addTask = ()=>{
-        if(input.value == ""){
-            input.style.border = "1px solid red"
-            return
-        }
+    addNewTask(input.value);
+    clearTask();
+    addDisable = false;
+  };
 
-        const clearTask = ()=>{
-            input.value = "Add a new task"
-            input.style.border = "none"
-            const createAddBtn = document.querySelector(".task-btn")
-            createAddBtn.parentNode.removeChild(createAddBtn)
-            
-        }
+  const addNewTask = (value) => {
+    const taskContainer = createDiv();
+    taskContainer.classList.add("task-container");
+    contentsContainer.appendChild(taskContainer);
 
-        addNewTask(input.value)
-        clearTask()
-        addDisable = false
-    }
+    const task = createDiv();
+    task.classList.add("task");
+    taskContainer.appendChild(task);
 
-    const addNewTask = (value)=>{
-        
-        const taskContainer = createDiv()
-        taskContainer.classList.add("task-container")
-        contentsContainer.appendChild(taskContainer)
+    const input = createInput();
+    input.value = value;
+    input.readOnly = true;
 
-        const task = createDiv()
-        task.classList.add("task")
-        taskContainer.appendChild(task)
+    task.appendChild(input);
 
-        const input = createInput()
-        input.value = value
-        input.readOnly = true
+    const editDiv = createDiv();
+    editDiv.classList.add("edit");
+    taskContainer.appendChild(editDiv);
 
-        task.appendChild(input)
+    const editBtn = createI();
+    editBtn.classList.add("fa-solid", "fa-pen-to-square");
 
-        const editDiv = createDiv()
-        editDiv.classList.add("edit")
-        taskContainer.appendChild(editDiv)
-    
-        const editBtn = createI()
-        editBtn.classList.add("fa-solid","fa-pen-to-square")
-        
-        const deleteBtn = createI()
-        deleteBtn.classList.add("fa-solid","fa-trash")
-        
-        const saveBtn = createButton()
-        saveBtn.classList.add("save-btn")
-        saveBtn.textContent = "Save"
-        editDiv.appendChild(editBtn)
-        editDiv.appendChild(deleteBtn)
-        editDiv.appendChild(saveBtn)
+    const deleteBtn = createI();
+    deleteBtn.classList.add("fa-solid", "fa-trash");
 
+    const saveBtn = createButton();
+    saveBtn.classList.add("save-btn");
+    saveBtn.textContent = "Save";
+    editDiv.appendChild(editBtn);
+    editDiv.appendChild(deleteBtn);
+    editDiv.appendChild(saveBtn);
 
-        const editNewTask = (e)=>{
-            const input = e.target.parentNode.previousSibling.querySelector("input")
-            const editBtn = e.target
-            const deleteBtn = e.target.nextSibling
-            const saveBtn = deleteBtn.nextSibling
-            
-            editBtn.style.display = "none"
-            deleteBtn.style.display = "none"
-            saveBtn.style.display = "block"
-            input.readOnly = false
-            input.style.border = "1px solid green"
-            
-            const saveEdit = ()=>{
-                editBtn.style.display = "block"
-                deleteBtn.style.display = "block"
-                saveBtn.style.display = "none"
-                input.readOnly = true
-                input.style.border = "none"
-            }
+    const editNewTask = (e) => {
+      const input = e.target.parentNode.previousSibling.querySelector("input");
+      const editBtn = e.target;
+      const deleteBtn = e.target.nextSibling;
+      const saveBtn = deleteBtn.nextSibling;
 
-            saveBtn.addEventListener("click",saveEdit)
-        }
+      editBtn.style.display = "none";
+      deleteBtn.style.display = "none";
+      saveBtn.style.display = "block";
+      input.readOnly = false;
+      input.style.border = "1px solid green";
 
-        const deleteTask = (e)=>{
-            const container =  e.target.parentElement.parentElement
-            container.parentNode.removeChild(container)
-         }
+      const saveEdit = () => {
+        editBtn.style.display = "block";
+        deleteBtn.style.display = "block";
+        saveBtn.style.display = "none";
+        input.readOnly = true;
+        input.style.border = "none";
+      };
 
-        editBtn.addEventListener("click",editNewTask)
-        deleteBtn.addEventListener("click",deleteTask)
-        
-        
+      saveBtn.addEventListener("click", saveEdit);
+    };
 
-        // Addeventlistener
-    }
+    const deleteTask = (e) => {
+      const container = e.target.parentElement.parentElement;
+      container.parentNode.removeChild(container);
+    };
 
+    editBtn.addEventListener("click", editNewTask);
+    deleteBtn.addEventListener("click", deleteTask);
 
-    // Addeventlister
-    plus.addEventListener("click",editTask)
+    // Addeventlistener
+  };
 
-  
-}
+  // Addeventlister
+  plus.addEventListener("click", editTask);
+};
 
-
-
-export const contentsDisplay = (title) =>{
-    contentsContainer.innerHTML = ""
-    setH1Title(title)
-    createTask()
-}
+export const contentsDisplay = (title) => {
+  contentsContainer.innerHTML = "";
+  setH1Title(title);
+  createTask();
+};
